2ddb7c6bebe1a37e3bef40220f556f8da61c4c39
霁月 <mickey.fetchbot@gmail.com>
2010-07-06 22:33:03 +0800

    add tcpl ex 2-3

--- laiyonghao-functions.php
+++ laiyonghao-functions.php
@@ -3,7 +3,7 @@ function the_video_url($post_id) {
   $post = get_post($post_id);
   if(!empty($post)) {
     $content = $post->post_content;
-    $r = "/<embed type=\"application\/x-shockwave-flash\" width=\"\d.*?\" height=\"\d.*?\" src=\"(\S.*?)\" \S.*?>/i";
+    $r = "/<embed \s.* height=\"\d.*?\" src=\"(\S.*?)\" \S.*?>/i";
     if(preg_match($r,$content,$match))
       print_r($match);
   }
